f529a69a231593d9e47317d566f0e5ce3b42ce4e
arjunsatarkar <87096069+aps-0@users.noreply.github.com>
2022-12-02 09:17:42 +0530

    Initialise repo and day 1

---
+++ 1/solution.py
@@ -0,0 +1,11 @@
+with open("input.txt", "r") as input_file:
+	input = input_file.read().strip()
+
+data = []
+for elf_calories in input.split("\n\n"):
+	data.append([int(item_calories) for item_calories in elf_calories.split("\n")])
+
+elf_total_calories = sorted([sum(elf_calories) for elf_calories in data], reverse=True)
+
+print("Part 1:", elf_total_calories[0])
+print("Part 2:", sum(elf_total_calories[0:3]))
---
+++ README.txt
@@ -0,0 +1,3 @@
+My solutions for Advent of Code 2022 <https://adventofcode.com/2022/>
+
+Made by Arjun Satarkar. I place all code in this repository under the CC0 1.0 Universal public domain dedication (see COPYING.txt).
